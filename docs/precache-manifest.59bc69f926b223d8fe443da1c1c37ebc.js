d72b39196a393a073a053c3892b6a620c13bc504
Felipe Valderrama <felipe.evs@gmail.com>
2020-10-06 21:37:59 -0300

    docs agregado

---
+++ docs/precache-manifest.59bc69f926b223d8fe443da1c1c37ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4124b629d82eea28709fea969270759",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e24d19ba2b91083f97",
+    "url": "/static/css/main.ae1a06a8.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e24d19ba2b91083f97",
+    "url": "/static/js/main.09789e1f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
